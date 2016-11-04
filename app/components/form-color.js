e6c10ce0accd5a46714efeb2af13bcb538d41727
Michel Boaventura <michel.boaventura@gmail.com>
2016-11-04 15:09:09 -0200

    form-color

--- app/components/form-color.js
+++ app/components/form-color.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  init() {
+    this._super(...arguments);
+    this.set('parsedValues', JSON.parse(this.get('field.values')));
+  }
 });
