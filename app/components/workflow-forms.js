244823d3f36dff9a195ed5bb397f5c8d27080410
Michel Boaventura <michel.boaventura@gmail.com>
2016-11-06 17:26:52 -0200

    Saving using button

--- app/components/workflow-forms.js
+++ app/components/workflow-forms.js
@@ -4,10 +4,12 @@ export default Ember.Component.extend({
   actions: {
     formChanged(key, value) {
       this.set(`filledForms.${key}`, value);
-      this.get('workflow').save();
       if(key === 'color') {
         Ember.$('#' + this.get('task').id).css('background-color', value);
       }
+    },
+    saveForm() {
+      this.get('workflow').save();
     }
   }
 });
