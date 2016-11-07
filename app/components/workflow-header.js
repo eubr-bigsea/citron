35632137c5394cb6edda80307075602bf8ca3798
Michel Boaventura <michel.boaventura@gmail.com>
2016-11-07 11:29:58 -0200

    Revert "Mg workflowlist"

--- app/components/workflow-header.js
+++ app/components/workflow-header.js
@@ -3,9 +3,6 @@ import Ember from 'ember';
 const { inject: { service }, Component } = Ember;
 
 export default Component.extend({
-  classNames: ['profile', 'dropdown'],
-  tagName: 'li',
-
   session: service('session'),
   currentUser: service('current-user'),
   isEditing: false,
--- app/components/workflows/table-header.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  classNames: ['item', 'item-list-header', 'hidden-sm-down'],
-  tagName: 'li',
-
-});
