43827596a37da7c43819451136acf97720db44ee
Michel Boaventura <michel.boaventura@gmail.com>
2016-11-25 16:02:38 -0200

    New serializer to fix change in api

--- app/serializers/workflow.js
+++ app/serializers/workflow.js
@@ -1,6 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.JSONSerializer.extend({
+  normalizeFindAllResponse(store, primaryModelClass, payload, id, requestType) {
+    return this._super(store, primaryModelClass, payload.data, id, requestType);
+  },
   normalizeSaveResponse(store, primaryModelClass, payload, id, requestType) {
     let myPayload = requestType === "createRecord" ? payload : payload.data;
 
