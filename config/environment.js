48d0c81ae0f18ee3770e605d9fc82f85fd49be15
Michel Boaventura <michel.boaventura@gmail.com>
2016-12-12 15:22:09 -0200

    Updating config

--- config/environment.js
+++ config/environment.js
@@ -50,7 +50,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.thorn = process.env.THORN || 'http://localhost:3000';
+    ENV.thorn = 'http://citron.ctweb.inweb.org.br:3000';
   }
 
   return ENV;
