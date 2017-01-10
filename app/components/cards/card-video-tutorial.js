4f1ed577212a5503827a5a251050d2e29dbecc1b
Matheus Gonçalves <souzagonc@gmail.com>
2017-01-10 12:41:10 -0200

    implement card release notes, with info of the realesed version

--- app/components/cards/card-video-tutorial.js
+++ app/components/cards/card-video-tutorial.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['col-xs-12', 'col-sm-12', 'col-md-7', 'col-lg-6', 'col-xl-5'],
 });
--- app/routes/home/index.js
+++ app/routes/home/index.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    var cards = {
+      releaseNotes: {
+        message: "This is the version 2.0 of application Lemonade! With new operations, faster and simple to use this version brings the power of Apache Spark to execute data minning as easy as build a flow." ,
+        version: "2.0",
+        date: "01/01/2017"},
+      videoTutorial: {
+        title: "Basic Tutorial",
+        youtubeID: "5_sHXJC9ocA"
+      }
+    };
+    return cards;
+  },
 });
