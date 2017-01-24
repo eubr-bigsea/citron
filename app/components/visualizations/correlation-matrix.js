5724a1c5c7ff1434abf17521de766c41d77d69f0
Geraldo Ribeiro Franciscani <gfranciscani@gmail.com>
2017-01-24 15:04:41 -0200

    Adding real data to correlation-matrix

--- app/components/visualizations/correlation-matrix.js
+++ app/components/visualizations/correlation-matrix.js
@@ -1,15 +1,18 @@
 import Ember from 'ember';
 
+
+
 export default Ember.Component.extend({
   init() {
     this._super(...arguments);
   },
 
   // Attributes bingins
-  width:  function(){ return this.get('width'); }.property('width'),
-  height: function(){ return this.get('height'); }.property('height'),
-  _id:    function(){ return this.get('_id'); }.property('_id'),
-  style:  function(){ return "width:"+this.get('width')+"; height:"+this.get('height')+";"; }.property('style'),
+  dataUrl: function(){ return this.get('dataUrl'); }.property('dataUrl'),
+  width:    function(){ return this.get('width'); }.property('width'),
+  height:   function(){ return this.get('height'); }.property('height'),
+  _id:      function(){ return this.get('_id'); }.property('_id'),
+  style:    function(){ return "width:"+this.get('width')+"; height:"+this.get('height')+";"; }.property('style'),
 
   // Chart var
   _var: null,
@@ -19,15 +22,40 @@ export default Ember.Component.extend({
 
     // Initialize variables
     let component = this;
+    let dataUrl = this.get('dataUrl');
+    //let dataUrl = "https://raw.githubusercontent.com/d3/d3-plugins/master/graph/data/miserables.json";
 
-    let data_url = "https://raw.githubusercontent.com/d3/d3-plugins/master/graph/data/miserables.json"
+    // Get data from API
+    $.ajax({
+      url: dataUrl,
+      type: "POST",
+      beforeSend() { gViz.helpers.loading.show(); },
+      contentType: "application/json",
+      //data: JSON.stringify({}),
+      success(data) {
 
-    component._var = gViz.vis.correlation_matrix()
-      ._var(component._var)
-      ._class("correlation-matrix-chart")
-      .container(".gViz-wrapper[data-id='"+component.get('_id')+"']")
-      .data(our_random_data[data_index])
-      .build();
+        component._var = gViz.vis.correlation_matrix()
+          ._var(component._var)
+          ._class("correlation-matrix-chart")
+          .container(".gViz-wrapper[data-id='"+component.get('_id')+"']")
+          .data(data)
+          .build();
+
+      },
+
+      // Hide loading div and render error
+      error() {
+        gViz.helpers.loading.hide();
+        console.log("Error");
+      },
+
+      // Hide loading div and render complete
+      complete() {
+        gViz.helpers.loading.hide();
+        //console.log("complete");
+      }
+    });
+
   },
 
   didInsertElement: function(){
