46a004c851616117fec70c3a4ee8c0da73013b1d
Yuri <yuricarvalhop@gmail.com>
2017-01-25 12:25:07 -0200

    Fixed errors, that were quoted on PR

--- app/components/workflow/workflow-form.js
+++ app/components/workflow/workflow-form.js
@@ -1,13 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  didInsertElement(){
-    Ember.$('.image-container').on('click',function(e){
-      e.preventDefault();
-      Ember.$('.image-container').removeClass('active');
-      Ember.$('.image-container').parents('.image-container').removeClass('active');
-      Ember.$(this).addClass('active');
-      Ember.$(this).parents('.image-container').addClass('main');
-    });
-  }
 });
--- app/routes/workflow/new.js
+++ app/routes/workflow/new.js
@@ -15,24 +15,25 @@ export default Ember.Route.extend({
       flows: [],
       platform: {id: '1'},
       user: currentUser,
-      image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSCygJA-ekdyncq1BI1okQKzwnSo-JEad-yFyI5LOfNYE874K50",
+      image: "img0.png",
       name: "My new Workflow",
       description: "My workflow..."
     };
     return RSVP.hash({
       workflow: this.get('store').createRecord('workflow', defaultWorkflow),
       platforms: this.store.query('platform', { enabled: true} ),
-      images: [
-        {name: img0.png,id: 0},
-        {name: img1.png,id: 1},
-        {name: img0.jpg,id: 0},
-        {name: img0.png,id: 0},
-        {name: img0.png,id: 0},
-        {name: img0.png,id: 0},
-        {name: img0.png,id: 0},
-        {name: img0.png,id: 0},
-        {name: img0.png,id: 0},
-
+      images:
+      [
+        {name: 'img0.png', id: 0},
+        {name: 'img1.png', id: 1},
+        {name: 'img2.jpg', id: 2},
+        {name: 'img3.jpg', id: 3},
+        {name: 'img4.jpg', id: 4},
+        {name: 'img5.jpg', id: 5},
+        {name: 'img6.jpg', id: 6},
+        {name: 'img7.jpg', id: 7},
+        {name: 'img8.jpg', id: 8},
+        {name: 'img9.png', id: 9},
       ]
     });
   },
@@ -42,12 +43,15 @@ export default Ember.Route.extend({
       var workflow = this.currentModel.workflow;
       var platform_id = Ember.$("#platform").val();
       workflow.set('platform.id', platform_id);
-      if(Ember.$(".image-container.active > .image").attr('style')){
-        this.set('currentModel.image', Ember.$(".image-container.active > .image").attr('style').slice(22, -1).replace(/"/g, "").replace(/'/g,""));
-      }
       workflow.save().then(() => {
         this.transitionTo('workflow.draw', workflow.get('id'), { queryParams: {platform: platform_id}} );
       });
     },
+    selectImage(id, name){
+      Ember.$('.image-container').removeClass('active');
+      Ember.$(`#img${id}`).addClass('active');
+      var workflow = this.currentModel.workflow;
+      workflow.set('image', name)
+    }
   },
 });
