df758ee2c2c67f668868b2422d2a1dd3122ade18
Pedro Bernardina <pedro.nascimento@dcc.ufmg.br>
2017-02-21 13:35:48 -0300

    Changed sentiment table

--- app/components/visualizations/sentimentos/table-chart.js
+++ app/components/visualizations/sentimentos/table-chart.js
@@ -16,18 +16,66 @@ export default Ember.Component.extend({
 
   // Chart var
   _var: null,
-
   _data:null,
+  _sort_state: {name: "asc", value: "desc"},
 
   actions: {
 
-    sortTable(index) {
+    sortTable(type) {
+      let component = this;
+      let sort_state = component.get("_sort_state");
 
+      let table = document.getElementById(component.get("_id"));
+      let rows = table.getElementsByClassName("row");
+
+      let sorted_rows = $(rows).sort(function(a, b) {
+
+        let val_a = $(a).attr(`sort-by-${type}`);
+        let val_b = $(b).attr(`sort-by-${type}`);
+
+        switch(type) {
+          case "name":
+            val_a = val_a.toLowerCase();
+            val_b = val_b.toLowerCase();
+            break;
+          case "value":
+            val_a = parseFloat(val_a);
+            val_b = parseFloat(val_b);
+            break;
+        }
+
+        // Returns sort in ascending or descending order
+        // according to the current sort state.
+        // The first sort by name is alphabetically,
+        // while the first sort by value is descending, and therefore,
+        // the biggest values are shown on top
+        switch(sort_state[type]) {
+          case "asc":
+            return (val_a < val_b) ? -1 : (val_a > val_b) ? 1 : 0;
+          case "desc":
+            return (val_a < val_b) ? 1 : (val_a > val_b) ? -1 : 0;
+        }
+      })
+
+      // After the sorting is done, changes the sort type
+      // Therefore, sorting type alternates between ascending and
+      // descending everytime the user clicks on the button
+      switch(sort_state[type]) {
+        case "asc":
+          sort_state[type] = "desc";
+          component.set("_sort_state", sort_state);
+          break;
+        case "desc":
+          sort_state[type] = "asc";
+          component.set("_sort_state", sort_state);
+          break;
+      }
+
+      $(table).html(sorted_rows);
     }
   },
 
   // Draw Chart
-
   didInsertElement: function(){
 
     let component = this;
@@ -74,17 +122,21 @@ export default Ember.Component.extend({
 
             d["avg"] = ((d["sum"]/d["total_num"]) * 100).toFixed(2);
 
+            // Gets only numerical values of RGB colours
             var colours = colors.scale(d["avg"]/100);
             var coloursOnly = colours.substring(colours.indexOf('(') + 1, colours.lastIndexOf(')')).split(",");
 
+            // Adds opacity to RGB colour
             var colourString = `rgba(${coloursOnly[0]},${coloursOnly[1]},${coloursOnly[2]}, 0.6)`;
 
+            // Sets width and colour of progress bar
             d["style"] = `
-            width:${d["avg"]}%;
-            background-color:${colourString};
+              width:${d["avg"]}%;
+              background-color:${colourString};
             `;
           });
 
+          // First ordenation is by alphabetically order
           data[attr].sort(function(a, b) { return d3.ascending(a.name, b.name); });
         };
 
