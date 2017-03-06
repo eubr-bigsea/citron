36379b26925326cf545458fac3ef9f771eaa224b
Matheus Gonçalves <souzagonc@gmail.com>
2017-03-06 15:11:24 -0300

    showing only workflows enabled

--- app/routes/home/workflows.js
+++ app/routes/home/workflows.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   currentUser: Ember.inject.service('current-user'),
   model() {
-    return this.store.query('workflow', {user_id: this.get('currentUser').id});
+    return this.store.query('workflow', {user_id: this.get('currentUser').id, enabled: true, platform: 'spark'});
   },
 });
