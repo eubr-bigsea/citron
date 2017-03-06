0e17774c2006b37039943e7c1a942e1a63bd2aa7
Matheus Gonçalves <souzagonc@gmail.com>
2017-03-06 17:58:49 -0300

    better design and funcs to optionbar

--- app/components/workflows/workflow-draw.js
+++ app/components/workflows/workflow-draw.js
@@ -1,8 +1,12 @@
 import Ember from 'ember';
+import Ps from 'npm:perfect-scrollbar';
 
 export default Ember.Component.extend({
   classNames: ['row'],
   forms: Ember.Object.create(),
   task: null,
   filledForms: Ember.Object.create(),
+  didInsertElement(){
+    Ps.initialize(document.getElementById("lemonade-container"));
+  }
 });
--- app/controllers/workflow/draw.js
+++ app/controllers/workflow/draw.js
@@ -4,15 +4,41 @@ export default Ember.Controller.extend({
   queryParams:['platform'],
   platform: null,
   targetName: null,
+  hasChanged: false,
+  message: {
+    type: null,
+    content: null,
+    func: null
+  },
 
   actions: {
+    hasChanged(param){ this.set('hasChanged', param); },
+    alert(param){
+      this.set('message', param);
+      $('#confirm-modal').addClass('show');
+    },
     confirmedTransition(){
       this.transitionToRoute(this.get('targetName'));
       this.set('targetName', null);
+      this.set('hasChanged', false);
     },
     canceledTransition(){
       this.set('targetName', null);
-      Ember.$('#confirm-modal').removeClass('show');
+      $('#confirm-modal').removeClass('show');
+    },
+    deleteWorkflow(){
+      $('#confirm-modal').removeClass('show');
+      var controller = this;
+      this.get('model.workflow').destroyRecord().then(function(){
+        controller.set('hasChanged', false);
+        var params = {
+          type: 'Success',
+          content:'Workflow has been deleted.',
+          func: "Ember.getOwner(component).lookup('router:main').transitionTo('home.workflows');"
+        };
+        controller.set('message', params);
+      $('#confirm-modal').addClass('show');
+      });
     },
   },
 });
