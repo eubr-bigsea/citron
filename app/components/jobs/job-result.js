f74142ee3ad43686cba0b5eece747227cf68d5bb
Michel Boaventura <michel.boaventura@gmail.com>
2017-03-10 13:00:52 -0300

    Removendo result temporário

--- app/components/jobs/job-result.js
+++ app/components/jobs/job-result.js
@@ -1,23 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  myResults: Ember.A([
-    {
-      "type": "VISUALIZATION",
-      "task": { "id": "4b7dcbe8-7b7e-4ca9-96ca-db8cdb4821f0" },
-      "operation": { "id": 71 }
-    },
-    {
-      "type": "VISUALIZATION",
-      "task": { "id": "97d698e6-45f3-4416-8135-6636828b52ab" },
-      "operation": { "id": 69 }
-    },
-    {
-      "type": "VISUALIZATION",
-      "task": { "id": "f540e4c6-99fc-4fca-906a-ab0b51b2d3d7" },
-      "operation": { "id": 71 }
-    }
-  ]),
   init() {
     this._super(...arguments);
 
