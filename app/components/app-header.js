f9901f7e48c5eac88802b812f156f2b23561b656
Matheus Gonçalves <souzagonc@gmail.com>
2017-03-20 17:22:55 -0300

    rework of app header

--- app/components/app-header.js
+++ app/components/app-header.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+const { inject: { service }, Component } = Ember;
+
+export default Component.extend({
+  session: service('session'),
+  currentUser: service('current-user'),
+
+  actions: {
+    invalidateSession(){
+      this.get('session').invalidate();
+    }
+  }
 });
