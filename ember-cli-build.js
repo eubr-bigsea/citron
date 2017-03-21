8edd1a66db1a98142897f8b0708b64e8963bc882
Pedro Bernardina <pedronascimento@dcc.ufmg.br>
2017-03-21 12:18:33 -0300

    moved libs from /public to /vender. Importing them in ember-cli-build.js and removed their script tag from index.html

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -27,5 +27,16 @@ module.exports = function(defaults) {
   app.import('vendor/select2/select2.full.js');
   app.import('vendor/select2/select2.css');
 
+  // Visualizations Required Libraries
+  app.import('vendor/gViz/libs/js/bootstrap-slider.js');
+  app.import('vendor/gViz/libs/css/bootstrap-slider.css');
+
+  app.import('vendor/gViz/libs/js/tooltipster.bundle.min.js');
+  app.import('vendor/gViz/libs/css/tooltipster.bundle.min.css');
+
+  app.import('vendor/gViz/libs/js/d3.v4.min.js');
+  app.import('vendor/gViz/libs/js/d3.position.js');
+  app.import('vendor/gViz/libs/js/latinise_compact.js');
+
   return app.toTree();
 };
