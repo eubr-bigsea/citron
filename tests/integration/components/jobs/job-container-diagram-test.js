23376fcc95c374683089ef3fc905e0fbd189c0d9
Matheus Gonçalves <souzagonc@gmail.com>
2017-03-29 19:30:01 -0300

    first repaginate finish

--- tests/integration/components/jobs/job-container-diagram-test.js
+++ tests/integration/components/jobs/job-container-diagram-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('jobs/job-container', 'Integration | Component | jobs/job container', {
+moduleForComponent('jobs/job-container-diagram', 'Integration | Component | jobs/job container diagram', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{jobs/job-container}}`);
+  this.render(hbs`{{jobs/job-container-diagram}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#jobs/job-container}}
+    {{#jobs/job-container-diagram}}
       template block text
-    {{/jobs/job-container}}
+    {{/jobs/job-container-diagram}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
