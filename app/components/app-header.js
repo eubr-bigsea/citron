3a1336fe953fbc5498079c46fa05747da69fa628
Michel Boaventura <michel.boaventura@gmail.com>
2017-05-09 14:44:24 -0300

    Refactoring views

--- app/components/app-header.js
+++ app/components/app-header.js
@@ -3,12 +3,11 @@ import Ember from 'ember';
 const { inject: { service }, Component } = Ember;
 
 export default Component.extend({
-  i18n: Ember.inject.service(),
+  i18n: service(),
   classNames: ['language-select'],
-  
   session: service('session'),
   currentUser: service('current-user'),
-  
+
   locales: Ember.computed('i18n.locale', 'i18n.locales', function() {
     const i18n = this.get('i18n');
     return this.get('i18n.locales').map(function (loc) {
--- app/components/cards/item-list-workflow.js
+++ app/components/cards/item-list-workflow.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  
+
   store: Ember.inject.service('store'),
   classNames:['table-item'],
   tagName: 'tr',
