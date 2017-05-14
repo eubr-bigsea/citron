cc6d5dae7b33a427298cf3c4a1c7c6c1c53caad1
Walter dos Santos Filho <waltersf@gmail.com>
2017-05-14 20:21:06 +0000

    Another try

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -15,14 +15,13 @@ module.exports = function(defaults) {
     fingerprint: {
       exclude: ['assets/images'],
     },
-/*
     minifyJS: {
       options: {
         exclude: ["assets/js/gViz/**", 
                   "assets/js/gViz", 
                   "public/assets/js/gViz/**"]
       }
-    }*/
+    }
   });
 
   app.import('vendor/metisMenu/metisMenu.js');
@@ -37,15 +36,15 @@ module.exports = function(defaults) {
   app.import('vendor/select2/select2.css');
 
   // Visualizations Required Libraries
-  app.import('vendor/gVis/libs/bootstrap-slider/bootstrap-slider.js');
-  app.import('vendor/gVis/libs/bootstrap-slider/bootstrap-slider.css');
+  app.import('vendor/gViz/libs/bootstrap-slider/bootstrap-slider.js');
+  app.import('vendor/gViz/libs/bootstrap-slider/bootstrap-slider.css');
 
-  app.import('vendor/gVis/libs/tooltipster/tooltipster.bundle.min.js');
-  app.import('vendor/gVis/libs/tooltipster/tooltipster.bundle.min.css');
+  app.import('vendor/gViz/libs/tooltipster/tooltipster.bundle.min.js');
+  app.import('vendor/gViz/libs/tooltipster/tooltipster.bundle.min.css');
 
-  app.import('vendor/gVis/libs/d3.v4/d3.v4.min.js');
-  app.import('vendor/gVis/libs/d3.position/d3.position.js');
-  app.import('vendor/gVis/libs/latinise/latinise_compact.js');
+  app.import('vendor/gViz/libs/d3.v4/d3.v4.min.js');
+  app.import('vendor/gViz/libs/d3.position/d3.position.js');
+  app.import('vendor/gViz/libs/latinise/latinise_compact.js');
 
   return app.toTree();
 };
