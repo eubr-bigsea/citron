e78c93355c2d2e009d73b80da19502a0ac1d87e8
Geraldo Ribeiro Franciscani <gfranciscani@gmail.com>
2017-06-05 19:48:16 -0300

    Adding visualizations list view

---
+++ app/components/visualizations/vis-wrapper.js
@@ -0,0 +1,50 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  init() {
+    this._super(...arguments);
+  },
+
+  // Set html elements
+  tagName: "div",
+  classNames: ["gViz-outer-wrapper"],
+
+  // Initialize data
+  data: null,
+
+  // Initialize data
+  didInsertElement() {
+
+    // Update data function
+    this.get('updateData')(this);
+
+  },
+
+  // Update data function
+  updateData: function(self) {
+
+    // Get data from API
+    $.ajax({
+      url: self.get('dataUrl'),
+      type: "GET",
+      beforeSend() { gViz.helpers.loading.show(); },
+      contentType: "application/json",
+      success(json) {
+
+        // Update data
+        self.set('data', json);
+
+      },
+
+      // Hide loading div and render error
+      error() { gViz.helpers.loading.hide(); console.log("Error"); },
+
+      // Hide loading div and render complete
+      complete() { gViz.helpers.loading.hide(); }
+
+    });
+
+  }
+
+});
