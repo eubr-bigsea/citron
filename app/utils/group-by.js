e7780c22fa3fa0b6cbff540fbac72d7cb5a51706
Yuri <yuricarvalhop@gmail.com>
2017-06-06 13:29:25 -0300

    Levelling working, need to be sorted

--- app/utils/group-by.js
+++ app/utils/group-by.js
@@ -1,16 +1,31 @@
 export default function groupBy(data, group) {
   return data.then(function(rawData) {
     let el = {};
+     let types = {
+      "subgroup": 2,
+      "group": 1
+    };
+    let others = ["technology", "model", "service", "data source", "algorithm"];
 
+      var allTypes = []
     rawData.forEach(function(o) {
-      let cat = o.get(group).filter(e => e.type === 'parent')[0];
-      let name = cat === undefined ? 'Others' : cat.name;
+      let myEl = el;
+      o.get(group).filter((a) => others.indexOf(a.type) === -1 )
+        .sort((a, b) => types[a.type] - types[b.type])
+        .forEach((e) => {
+        if(e.type !== 'group' && e.type !== 'subgroup'){
+        allTypes.push(e.type);
+        }
+          if(!myEl[e.name]) {
+            myEl[e.name] = {}
+          }
+          myEl = myEl[e.name];
+        });
 
-      if(el[name] === undefined) {
-        el[name] = [];
+      if(!myEl['_data']) {
+        myEl['_data'] = [];
       }
-
-      el[name].push(o);
+      myEl['_data'].push(o);
     });
     return el;
   });
