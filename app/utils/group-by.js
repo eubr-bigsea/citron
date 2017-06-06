af20e1ceaa30b84c2c3c5adbdce940bd9e17c339
Matheus Gonçalves <souzagonc@gmail.com>
2017-06-06 17:49:14 -0300

    sort by name in to levels

--- app/utils/group-by.js
+++ app/utils/group-by.js
@@ -1,23 +1,19 @@
 export default function groupBy(data, group) {
   return data.then(function(rawData) {
     let el = {};
-     let types = {
-      "subgroup": 2,
-      "group": 1
-    };
+    let types = { subgroup: 2, group: 1 };
     let others = ["technology", "model", "service", "data source", "algorithm"];
 
-      var allTypes = []
+    var groups = []
+
     rawData.forEach(function(o) {
       let myEl = el;
       o.get(group).filter((a) => others.indexOf(a.type) === -1 )
         .sort((a, b) => types[a.type] - types[b.type])
         .forEach((e) => {
-        if(e.type !== 'group' && e.type !== 'subgroup'){
-        allTypes.push(e.type);
-        }
           if(!myEl[e.name]) {
             myEl[e.name] = {}
+            groups.push(e.name)
           }
           myEl = myEl[e.name];
         });
@@ -27,6 +23,38 @@ export default function groupBy(data, group) {
       }
       myEl['_data'].push(o);
     });
-    return el;
+
+    var func = function(el){
+      var newArray = []
+
+      Object.keys(el).forEach(function(key){
+        var o = {}
+        o[key] = el[key]
+        newArray.push(o);
+      });
+
+      newArray = newArray.sort((a, b) => {
+        var nameA = Object.keys(a)[0];
+        var nameB = Object.keys(b)[0];
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        return 0;
+      });
+      return newArray;
+    }
+    groups = func(el);
+    groups.forEach(function(o){
+      Object.keys(o).forEach(function(obj){
+        if(o[obj].constructor.name === 'Object'){
+          o[obj] = func(o[obj]);
+        }
+      });
+    });
+debugger;
+    return groups;
   });
 }
