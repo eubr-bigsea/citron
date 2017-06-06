83f8643fe2f51ba92fd084485470386de76f9045
Geraldo Ribeiro Franciscani <gfranciscani@gmail.com>
2017-06-06 00:10:37 -0300

    Adding data visualization list layout

--- app/initializers/visualizations.js
+++ app/initializers/visualizations.js
@@ -3,21 +3,33 @@ import ENV from 'lemonade-ember/config/environment';
 export function initialize(/* application */) {
    ENV.APP.visualizations = {
      list: {
+       initials: "List",
        component: "visualizations/list-view",
-       icon: "list",
+       icon: "fa-list",
        title: "Visualizations",
        subtitle: "Available visualizations.",
        description: "List of available visualizations for the Lemonade framework.",
        dataUrl: "/assets/data/visualizations/pie-chart.json"
      },
      pie: {
+       initials: "Pie",
        component: "visualizations/pie-chart",
-       icon: "pie-chart",
+       icon: "fa-pie-chart",
        title: "Pie Chart",
        subtitle: "Part to whole",
        description: "A pie chart (or a circle chart) is a circular statistical graphic which is divided into slices to illustrate numerical proportion. In a pie chart, the arc length of each slice (and consequently its central angle and area), is proportional to the quantity it represents.",
        dataUrl: "/assets/data/visualizations/pie-chart.json"
+     },
+     map: {
+       initials: "Map",
+       component: "visualizations/map",
+       icon: "fa-map-marker",
+       title: "Map",
+       subtitle: "Geographic",
+       description: "A pie chart (or a circle chart) is a circular statistical graphic which is divided into slices to illustrate numerical proportion. In a pie chart, the arc length of each slice (and consequently its central angle and area), is proportional to the quantity it represents.",
+       dataUrl: "/assets/data/visualizations/pie-chart.json"
      }
+
    };
 }
 
