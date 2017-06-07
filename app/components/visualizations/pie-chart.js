c3073040b6c6e040bff09497fb66dd9e4f6e16f8
Geraldo Ribeiro Franciscani <gfranciscani@gmail.com>
2017-06-07 15:28:12 -0300

    Fix pull request reviewed by @michel

--- app/components/visualizations/pie-chart.js
+++ app/components/visualizations/pie-chart.js
@@ -1,16 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  init() {
-    this._super(...arguments);
-  },
 
   // Set html elements
-  tagName: "div",
   classNames: ["gViz-wrapper"],
-
-  // Set unique _id
-  _id: `visualization-${Math.floor(Math.random() * (1000000000 - 5 + 1)) + 5}`,
 
   // Chart var
   _var: null,
@@ -22,7 +15,7 @@ export default Ember.Component.extend({
       gViz.vis.pie_chart()
         ._var(this.get('_var'))
         ._class("pie-chart")
-        .container(`.gViz-wrapper-inner[data-id='${this.get('_id')}']`)
+        .container(`.gViz-wrapper-inner[data-id='${this.get('elementId')}']`)
         .data(this.get('data'))
         .build()
       );
