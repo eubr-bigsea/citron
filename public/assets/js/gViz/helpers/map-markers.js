e85c70f8230f7242048c54dee749507894bc2aff
Pedro Bernardina <pedronascimento@dcc.ufmg.br>
2017-06-07 11:04:45 -0300

    Creates JS for map

---
+++ public/assets/js/gViz/helpers/map-markers.js
@@ -0,0 +1,79 @@
+"use strict";
+
+// Create date helper main object
+
+if (!gViz.helpers.map_markers) {
+  gViz.helpers.map_markers = {};
+}
+
+gViz.helpers.map_markers.blue = new L.Icon({
+  iconUrl: 'assets/images/map-markers/marker-icon-2x-blue.png',
+  //shadowUrl: 'assets/images/map-markers/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+gViz.helpers.map_markers.red = new L.Icon({
+  iconUrl: 'assets/images/map-markers/marker-icon-2x-red.png',
+  //shadowUrl: 'assets/images/map-markers/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+gViz.helpers.map_markers.green = new L.Icon({
+  iconUrl: 'assets/images/map-markers/marker-icon-2x-green.png',
+  //shadowUrl: 'assets/images/map-markers/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+gViz.helpers.map_markers.orange = new L.Icon({
+  iconUrl: 'assets/images/map-markers/marker-icon-2x-orange.png',
+  //shadowUrl: 'assets/images/map-markers/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+gViz.helpers.map_markers.yellow = new L.Icon({
+  iconUrl: 'assets/images/map-markers/marker-icon-2x-yellow.png',
+  //shadowUrl: 'assets/images/map-markers/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+gViz.helpers.map_markers.violet = new L.Icon({
+  iconUrl: 'assets/images/map-markers/marker-icon-2x-violet.png',
+  //shadowUrl: 'assets/images/map-markers/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+gViz.helpers.map_markers.grey = new L.Icon({
+  iconUrl: 'assets/images/map-markers/marker-icon-2x-grey.png',
+  //shadowUrl: 'assets/images/map-markers/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+gViz.helpers.map_markers.black = new L.Icon({
+  iconUrl: 'assets/images/map-markers/marker-icon-2x-black.png',
+  //shadowUrl: 'assets/images/map-markers/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
