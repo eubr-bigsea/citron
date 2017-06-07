93448d4ae4c91f525cff6ff106ebb280b58c7e89
Michel Boaventura <michel.boaventura@gmail.com>
2017-06-07 13:33:54 -0300

    Improve port behaviour
    This commit implement the follow items:
    
    * Avoid self connections
    * Only show port labels when hover
    * Respect port multiplicity
    * Highlight possible connections according to port interfaces

--- app/models/operation.js
+++ app/models/operation.js
@@ -6,8 +6,10 @@ export default DS.Model.extend({
   name: attr('string'),
   icon: attr('string'),
   type: attr('string'),
+  multiplicity: attr('string'),
   slug: attr('string'),
   categories: attr(),
+  interfaces: attr(),
   ports: attr(),
   forms: attr(),
 });
