841fdd9afe45dcfbb1c3c450b6e59f2b39210c86
Pedro Bernardina <pedronascimento@dcc.ufmg.br>
2017-06-07 11:12:40 -0300

    Adds map component

--- app/components/visualizations/map-chart.js
+++ app/components/visualizations/map-chart.js
@@ -10,5 +10,24 @@ export default Ember.Component.extend({
   tagName: "div",
   classNames: ["gViz-wrapper"],
 
+  _id: `visualization-${Math.floor(Math.random() * (1000000000 - 5 + 1)) + 5}`,
 
+  // Main var
+  _var: null,
+
+  didRender: function(){
+
+    // Draw visualization
+    this.set('_var',
+      // Initializes map
+      gViz.vis.map()
+        ._class("map")
+        ._var(this.get('_var'))
+        .container(`.gViz-wrapper-inner[data-id='${this.get('_id')}']`)
+        .tile(`carto-light`)
+        .startPoint([-23.5275671, -46.5590158])
+        .data(this.get('data'))
+        .build()
+    );
+  }
 });
