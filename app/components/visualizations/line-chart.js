5d51692de81f904003c7181a472b9d7fe5c1aa34
Geraldo Ribeiro Franciscani <gfranciscani@gmail.com>
2017-06-07 15:28:12 -0300

    Fix pull request reviewed by @michel

--- app/components/visualizations/line-chart.js
+++ app/components/visualizations/line-chart.js
@@ -2,16 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  init() {
-    this._super(...arguments);
-  },
-
   // Set html elements
-  tagName: "div",
   classNames: ["gViz-wrapper"],
-
-  // Set unique _id
-  _id: `visualization-${Math.floor(Math.random() * (1000000000 - 5 + 1)) + 5}`,
 
   // Main var
   _var: null,
@@ -22,7 +14,7 @@ export default Ember.Component.extend({
     this.set('_var',
       gViz.vis.lineChart()
         ._var(this.get('_var'))
-        .container(`.gViz-wrapper-inner[data-id='${this.get('_id')}']`)
+        .container(`.gViz-wrapper-inner[data-id='${this.get('elementId')}']`)
         .data(this.get('data'))
         .build()
     );
--- app/components/visualizations/pie-chart.js
+++ app/components/visualizations/pie-chart.js
@@ -3,18 +3,9 @@ import Ember from 'ember';
 const { inject: { service } } = Ember;
 
 export default Ember.Component.extend({
-  session: service(),
-
-  init() {
-    this._super(...arguments);
-  },
 
   // Set html elements
-  tagName: "div",
   classNames: ["gViz-wrapper"],
-
-  // Set unique _id
-  _id: `visualization-${Math.floor(Math.random() * (1000000000 - 5 + 1)) + 5}`,
 
   // Chart var
   _var: null,
@@ -26,7 +17,7 @@ export default Ember.Component.extend({
       gViz.vis.pie_chart()
         ._var(this.get('_var'))
         ._class("pie-chart")
-        .container(`.gViz-wrapper-inner[data-id='${this.get('_id')}']`)
+        .container(`.gViz-wrapper-inner[data-id='${this.get('elementId')}']`)
         .data(this.get('data'))
         .build()
       );
