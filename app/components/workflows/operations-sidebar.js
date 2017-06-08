db677ea120000a1a7b4974b10ec6cc7ef7123bad
Matheus Gonçalves <souzagonc@gmail.com>
2017-06-08 15:40:32 -0300

    fix workflow operations list scrollbar on firefox

--- app/components/workflows/operations-sidebar.js
+++ app/components/workflows/operations-sidebar.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
+import Ps from 'npm:perfect-scrollbar';
 
 export default Ember.Component.extend({
   classNames: ['operation', 'sidebar', 'wrapper'],
   didInsertElement() {
+    Ps.initialize(document.getElementById("operations-list"));
     Ember.$('#categories-list').metisMenu();
   },
 
