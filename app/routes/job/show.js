1a89f488c841ebd0d7522cc9c88958b0ce39c3e2
Michel Boaventura <michel.boaventura@gmail.com>
2017-06-12 14:48:42 -0300

    Translate operations on job#show
    This commit send the 'lang' argument to the backend on job#show route.
    
    Fix #296

--- app/routes/job/show.js
+++ app/routes/job/show.js
@@ -1,12 +1,21 @@
 import Ember from 'ember';
 import RSVP from 'rsvp';
 
+const { inject: { service } } = Ember
+
 export default Ember.Route.extend({
+  i18n: service(),
+
   model(params) {
     this.controllerFor('job.show').set('stepsLogs', Ember.A());
+
+    let queryParams = {
+      lang: this.get('i18n.locale')
+    };
+
     return RSVP.hash({
       job: this.store.findRecord('job', params.id),
-      operations: this.store.query('operation', {})
+      operations: this.store.query('operation', queryParams)
     });
   },
 });
