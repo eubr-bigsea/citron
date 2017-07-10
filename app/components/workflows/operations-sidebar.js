6f3d8f424faefd673224b4e923e5975c54e3f982
Yuri <yuricarvalhop@gmail.com>
2017-07-10 08:40:12 -0300

    Search operation on operation-sidebar is now working

--- app/components/workflows/operations-sidebar.js
+++ app/components/workflows/operations-sidebar.js
@@ -2,15 +2,31 @@ import Ember from 'ember';
 import Ps from 'npm:perfect-scrollbar';
 
 export default Ember.Component.extend({
+  filterText: '',
   classNames: ['operation', 'sidebar', 'wrapper'],
+
+  filteredResults: Ember.computed('filterText', function() {
+    var filter = this.get('filterText').toString().toLowerCase();
+    return this.get('operations').filter(function(item){
+      return item.get('name').toString().toLowerCase().indexOf(filter) !== -1;
+    });
+  }),
+
   didInsertElement() {
     Ps.initialize(document.getElementById("operations-list"));
     Ember.$('#categories-list').metisMenu();
   },
 
+  didRender(){
+    if(this.get('filterText') === ''){
+      Ember.$('#categories-list').metisMenu('dispose');
+      Ember.$('#categories-list').metisMenu();
+    }
+  },
+
   actions: {
     toggleSidebar(){
       Ember.$("#page-content-wrapper").toggleClass('toggled');
-    }
+    },
   },
 });
