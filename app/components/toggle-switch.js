a178966118633cf12bf0abf4a44ac8306c24f7a0
Pedro Bernardina <pedrobernardina@Pedros-MacBook-Pro.local>
2017-08-21 18:21:51 -0300

    Minor changes to toggle and map css

--- app/components/toggle-switch.js
+++ app/components/toggle-switch.js
@@ -2,9 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  didInsertElement() {
+    var self = this;
+
+    if(self.get("checked")) { d3.select("#toggle").attr("checked", true); }
+  },
+
   actions: {
     toggle(){
       this.get('toggleAction')();
     }
   }
+
 });
