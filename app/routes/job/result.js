12b3613c136193ccfc44345a654ab5e912a3565a
Michel Boaventura <michel.boaventura@gmail.com>
2017-08-24 17:36:07 -0300

    Migrating code to new visualizations

--- app/routes/job/result.js
+++ app/routes/job/result.js
@@ -1,9 +1,19 @@
 import Ember from 'ember';
+import config from '../../config/environment';
 
 export default Ember.Route.extend({
   model(params) {
     this._super(...arguments);
-    params.job = this.store.findRecord('job',params.jobId);
+    params.job = this.store.findRecord('job', params.jobId);
+    params.dataUrl = [
+      config.caipirinha,
+      "visualizations",
+      params.jobId,
+      params.taskId
+    ].join('/');
+
+    params.component = params.visu;
+
     return params;
   }
 });
