1a8c353df280aaa64af3832594cf60fec833ae78
Yuri <yuricarvalhop@gmail.com>
2017-08-30 12:31:01 -0300

    Default options changedw

--- app/components/forms/form-checkbox.js
+++ app/components/forms/form-checkbox.js
@@ -1,8 +1,27 @@
 import FormComponent from 'lemonade-ember/lib/form-component';
 
 export default FormComponent.extend({
-  init() {
+  didReceiveAttrs() {
     this._super(...arguments);
-    this.set('currentValue', this.get('currentValue') === true);
+    let isChecked;
+    if(this.get('currentValue') === '1' || this.get('currentValue') === true ){
+      this.set('isChecked', true);
+    } else {
+      this.set('isChecked', false);
+    }
+  },
+  actions:{
+    valueChanged(newValue){
+      if(newValue === true){
+        this.set('currentValue', '1');
+      } else {
+        this.set('currentValue', '0');
+      }
+      let onValueChanged = this.get('onValueChanged');
+
+      if(onValueChanged !== undefined) {
+        onValueChanged(this.get('name'), this.get('currentValue'));
+      }
+    }
   }
 });
