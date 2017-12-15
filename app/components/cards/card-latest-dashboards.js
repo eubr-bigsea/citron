aa72f9f20d876464588481b707f8c44661187f54
Matheus Gonçalves <souzagonc@gmail.com>
2017-12-15 14:13:59 -0200

    add latest dashboards and datasources cards

---
+++ app/components/cards/card-latest-dashboards.js
@@ -0,0 +1,28 @@
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+
+export default Component.extend({
+  store: service(),
+  sessionAccount: service(),
+  i18n: service(),
+
+  didInsertElement(){
+    this.set('locale', this.get('i18n.locale'));
+    var cardId = this.get('conf.card-id');
+    var userId = this.get('sessionAccount.userId');
+
+    this.get('store').findRecord('card', cardId, { reload: true }).then((card) => {
+      this.set('card', card);
+      this.get('store').query('dashboard', {
+        user_id: userId,
+        enabled: true,
+        page: '1',
+        size: card.get('content.size'),
+        sort: 'updated',
+        asc: false
+      }).then((dashboards) => {
+        this.set('dashboards', dashboards)
+      });
+    });
+  }
+});
