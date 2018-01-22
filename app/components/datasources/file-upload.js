6662d6b3e7f7a0d1461ca2b4abaa8e549a23df3b
Michel Boaventura <michel.boaventura@gmail.com>
2018-01-22 19:48:42 -0200

    Migrating libs to use shims and node_modules

--- app/components/datasources/file-upload.js
+++ app/components/datasources/file-upload.js
@@ -5,6 +5,7 @@ import config from '../../config/environment';
 import generateUUID from 'lemonade-ember/utils/generate-uuid'
 import Resumable from '@resumable';
 import generateUUID from 'lemonade-ember/utils/generate-uuid'
+import Resumable from '@resumable';
 
 export default Component.extend({
   session: service(),
