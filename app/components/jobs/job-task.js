65d7c2c0d2eca3cdf9f5d059642ee1eff36c3ace
Matheus Gonçalves <souzagonc@gmail.com>
2018-03-14 16:38:31 -0300

    re-style job-task on show

--- app/components/jobs/job-task.js
+++ app/components/jobs/job-task.js
@@ -28,14 +28,8 @@ export default Component.extend({
           scope: port.interfaces.map(el => el.name).join(" "),
           anchors: anchorPosition(isInput, type.length, i),
           uuid: `${task.id}/${port.id}`,
-          endpoint: [ isInput ? "Dot" : "Rectangle", { radius: 5, width: 10, height: 10 } ],
-          overlays: [ [ "Label",
-            { label: port.name, id: "id", location: isInput ? [0, -1] : [0, 2], cssClass: "label-overlay" }
-          ] ],
-          connector: 'Flowchart',
-          connectorOverlays: [ [ "Arrow", { location: 0.75, length: 15, foldback: 0.8 } ] ],
+          endpoint: [ isInput ? "Dot" : "Rectangle", { radius: 7, width: 12, height: 12 } ],
           maxConnections: port.multiplicity === "ONE" ? 1 : -1,
-          dropOptions : { activeClass: isInput ? 'drag-input' : 'drag-output' },
         };
         this.get('endpoints').addObject(jsplumb.addEndpoint(el, opts));
       });
