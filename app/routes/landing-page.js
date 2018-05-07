8f4a2d7cd5ffdc142b9ef046be27617d7cfb4e0a
Matheus Gonçalves <souzagonc@gmail.com>
2018-05-07 12:26:15 -0300

    add en translations and logic to locale at landing-page

--- app/routes/landing-page.js
+++ app/routes/landing-page.js
@@ -1,6 +1,8 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  i18n: service(),
   setupController(){
     this.controllerFor('application').set('isLanding', true);
   },
