ee1003e4b7d8557de0a63216f73481164cd2d539
Matheus Gonçalves <souzagonc@gmail.com>
2018-05-31 20:16:34 -0300

    workaround to select2 auto order

--- app/components/forms/form-attribute-selector.js
+++ app/components/forms/form-attribute-selector.js
@@ -1,37 +1,52 @@
-import { set } from '@ember/object';
-import { A } from '@ember/array';
-import FormComponent from 'citron/lib/form-component';
-import { computed } from '@ember/object';
+import { set } from "@ember/object";
+import { A } from "@ember/array";
+import FormComponent from "citron/lib/form-component";
+import { computed } from "@ember/object";
+import $ from "jquery";
 
 export default FormComponent.extend({
-  classNameBindings: ['error'],
-  error: computed('field.error', function(){ return this.get('field.error') }),
+  classNameBindings: ["error"],
+  error: computed("field.error", function() {
+    return this.get("field.error");
+  }),
 
   didInsertElement() {
-    this.$('select').select2({
-      tags: true,
-      dropdownParent: this.$(),
-      data: this.get('parsedValues')
+    const component = this;
+    component.$("select").each(function() {
+      let $this = $(this);
+
+      $this.on("select2:select", function(e) {
+        let elm = e.params.data.element;
+        let $elm = $(elm);
+        let $t = $(this);
+        $t.append($elm);
+        $t.trigger("change.select2");
+      });
+      $this.select2({
+        tags: true,
+        dropdownParent: component.$(),
+        data: component.get("parsedValues")
+      });
     });
   },
 
-  didReceiveAttrs(){
-    this.set('parsedValues', A());
-    let parsed = this.get('parsedValues');
-    let uiPorts = this.get('task.uiPorts');
+  didReceiveAttrs() {
+    this.set("parsedValues", A());
+    let parsed = this.get("parsedValues");
+    let uiPorts = this.get("task.uiPorts");
     let values = null;
-    if(uiPorts && uiPorts.inputs){
-      let attributes = []
-      for(let i=0; i < uiPorts.inputs.length; i++){
-        attributes = attributes.concat(uiPorts.inputs[i].attributes)
+    if (uiPorts && uiPorts.inputs) {
+      let attributes = [];
+      for (let i = 0; i < uiPorts.inputs.length; i++) {
+        attributes = attributes.concat(uiPorts.inputs[i].attributes);
       }
-      values = [...new Set(attributes)]
+      values = [...new Set(attributes)];
     }
-    let currentValue = this.get('currentValue');
+    let currentValue = this.get("currentValue");
 
-    if(values) {
-      values.forEach((el) => {
-        if(parsed.findBy('id', el) === undefined) {
+    if (values) {
+      values.forEach(el => {
+        if (parsed.findBy("id", el) === undefined) {
           parsed.addObject({
             id: el,
             text: el,
@@ -41,25 +56,24 @@ export default FormComponent.extend({
       });
     }
 
-    if(currentValue) {
-      currentValue.forEach((el) => {
-        if(parsed.findBy('id', el) === undefined) {
+    if (currentValue) {
+      currentValue.forEach(el => {
+        if (parsed.findBy("id", el) === undefined) {
           parsed.addObject({
             id: el,
             text: el,
             selected: true
           });
         } else {
-          var current = parsed.findBy('id', el);
-          set(current, 'selected', true);
+          var current = parsed.findBy("id", el);
+          set(current, "selected", true);
         }
       });
     }
-
   },
   actions: {
     valueChanged() {
-      this._super(this.$('select').val());
+      this._super(this.$("select").val());
     }
   }
 });
